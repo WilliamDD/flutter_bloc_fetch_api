08d3565541b9e9d1d5ad35da1829b97243ef7c9a
william <w.dedooij@wylance.com>
2022-03-17 09:59:13 +0100

    initial

---
+++ lib/pages/covid_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/covid_bloc/covid_bloc.dart';
+import '../models/covid_model.dart';
+
+class CovidPage extends StatefulWidget {
+  const CovidPage({Key? key}) : super(key: key);
+
+  @override
+  _CovidPageState createState() => _CovidPageState();
+}
+
+class _CovidPageState extends State<CovidPage> {
+  final CovidBloc _newsBloc = CovidBloc();
+
+  @override
+  void initState() {
+    _newsBloc.add(GetCovidList());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('COVID-19 Lijst')),
+      body: _buildListCovid(),
+    );
+  }
+
+  Widget _buildListCovid() {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: BlocProvider(
+        create: (_) => _newsBloc,
+        child: BlocListener<CovidBloc, CovidState>(
+          listener: (context, state) {
+            if (state is CovidError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message!),
+                ),
+              );
+            }
+          },
+          child: BlocBuilder<CovidBloc, CovidState>(
+            builder: (context, state) {
+              if (state is CovidInitial) {
+                return _buildLoading();
+              } else if (state is CovidLoading) {
+                return _buildLoading();
+              } else if (state is CovidLoaded) {
+                return _buildCard(context, state.covidModel);
+              } else if (state is CovidError) {
+                return Container();
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, CovidModel model) {
+    return ListView.builder(
+      itemCount: model.countries!.length,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Container(
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Land: ${model.countries![index].country}"),
+                  Text(
+                      "Aantal positief: ${model.countries![index].totalConfirmed}"),
+                  Text("Aantal doden: ${model.countries![index].totalDeaths}"),
+                  Text(
+                      "Aantal herstelt: ${model.countries![index].totalRecovered}"),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildLoading() => const Center(child: CircularProgressIndicator());
+}
